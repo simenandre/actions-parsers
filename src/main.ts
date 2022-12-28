328dbe6a9c9ac75e26d0e9ed17397546cc64f234
Simen A. W. Olsen <so@bjerk.io>
2022-12-28 23:06:50 +0100

    fix: add missing exports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 export * from './parsers/core';
 export * from './parsers/literal';
 export * from './parsers/number';
+export * from './parsers/parsed';
+export * from './parsers/union';
 export * from './parsers/yaml';
